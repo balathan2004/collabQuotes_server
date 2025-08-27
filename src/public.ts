bf2d443b703e672986270ccd1a5ea1d4874063b5
light <balathan2004@gmail.com>
2025-08-27 21:05:21 +0530

    pagination changed

--- src/public.ts
+++ src/public.ts
@@ -7,7 +7,6 @@ import {
   UserDataInterface,
 } from "../utils/interfaces";
 import { QuoteInterface } from "../utils/interfaces";
-import ShortUniqueId from "short-unique-id";
 
 const PublicRoutes = Router();
 
@@ -21,21 +20,46 @@ PublicRoutes.get(
     const startIndex = page * limit;
     const endIndex = startIndex + limit;
 
-    console.log("req for page and limit", page, "   ", limit);
+    console.log("req for page and limit", startIndex, "   ", endIndex);
 
-    const usersData = (await supabase.from("users").select("*"))
-      .data as UserDataInterface[];
-    const data = (await supabase.from("posts").select("*"))
-      .data as QuoteInterface[];
+    // const usersData = (await supabase.from("users").select("*"))
+    //   .data as UserDataInterface[];
 
-    const merged: QuotesInterfaceWithProfile[] = data.map((post) => {
-      const currentUserData = usersData.filter(
+    // const data = (await supabase.from("posts").select("*"))
+    //   .data as QuoteInterface[];
+
+    // const merged: QuotesInterfaceWithProfile[] = data.map((post) => {
+    //   const currentUserData = usersData.filter(
+    //     (item) => item.userId == post.userId
+    //   );
+    //   return { ...post, profile_url: currentUserData[0].profile_url };
+    // });
+
+    // const paginatedPosts = merged.reverse().slice(startIndex, endIndex);
+
+    const posts = (
+      await supabase
+        .from("posts")
+        .select("*")
+        .order("createdAt", { ascending: false })
+        .range(startIndex, endIndex - 1)
+    ).data as QuoteInterface[];
+
+    const userIds = Array.from(new Set(posts.map((p) => p.userId)));
+
+    const usersData = (
+      await supabase.from("users").select("*").in("userId", userIds)
+    ).data as UserDataInterface[];
+
+    const merged: QuotesInterfaceWithProfile[] = posts.map((post) => {
+      const currentUserData = usersData.find(
         (item) => item.userId == post.userId
       );
-      return { ...post, profile_url: currentUserData[0].profile_url };
+
+      return { ...post, profile_url: currentUserData?.profile_url || "" };
     });
 
-    const paginatedPosts = merged.reverse().slice(startIndex, endIndex);
+    const paginatedPosts = merged;
 
     res.json({
       status: 200,
@@ -47,10 +71,26 @@ PublicRoutes.get(
 
 PublicRoutes.get("/get_one_random", async (req: Request, res: Response) => {
   try {
-    const data = (await supabase.from("posts").select("*"))
-      .data as QuoteInterface[];
+    const { count } = await supabase
+      .from("posts")
+      .select("*", { count: "exact", head: true });
 
-    const randomQuote = data[Math.floor(Math.random() * data.length)];
+    if (!count) {
+      res
+        .status(500)
+        .json({ message: "Quotes Not Found", status: 300, quote: "" });
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * count);
+
+    // Fetch random row
+    const { data } = await supabase
+      .from("posts")
+      .select("*")
+      .range(randomIndex, randomIndex);
+
+    const randomQuote = data;
 
     res.json({ message: "success", status: 200, quote: randomQuote });
   } catch (e) {
@@ -58,7 +98,6 @@ PublicRoutes.get("/get_one_random", async (req: Request, res: Response) => {
     res.status(500).json({ message: "error", status: 300, quote: "" });
   }
 });
-
 
 PublicRoutes.get(
   "/get_profile/:id",
@@ -93,7 +132,5 @@ PublicRoutes.get(
     }
   }
 );
-
-
 
 export default PublicRoutes;
